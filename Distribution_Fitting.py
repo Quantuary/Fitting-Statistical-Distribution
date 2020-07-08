e68f3dab0c7ea95bb8d0f2ee1faa0c381c1ec5d8
Quantuary <43050453+Quantuary@users.noreply.github.com>
2020-07-08 19:38:56 +1000

    upload documentations

--- Distribution_Fitting.py
+++ Distribution_Fitting.py
@@ -1,6 +1,5 @@
 """
-Created on Mon Jul  1 16:17:06 2019
-@author: marcus
+Created on Mon Jul  1 16:17:06 2020
 """
 
 
@@ -8,10 +7,8 @@ import warnings
 import numpy as np
 import pandas as pd
 import scipy.stats as st
-from scipy.stats import rv_continuous
 import matplotlib
 import matplotlib.pyplot as plt
-from scipy.special import gamma
 
 class fit_distribution():
 
@@ -19,40 +16,18 @@ class fit_distribution():
         self.data = data
         self.name =name
         
-    def best_fit_distribution(self, data, DISTRIBUTIONS, bins=250, ax=None):
+    def best_fit_distribution(self, data, dist, bins=250, ax=None):
         # Get histogram of original data
         y, x = np.histogram(data, bins=bins, density=True)
         x = (x + np.roll(x, -1))[:-1] / 2.0
     
-        # Distributions to check
-    # =============================================================================
-    #     DISTRIBUTIONS = [        
-    #         st.alpha,st.anglit,st.arcsine,st.beta,st.betaprime,st.bradford,st.burr,st.cauchy,st.chi,st.chi2,st.cosine,
-    #         st.dgamma,st.dweibull,st.erlang,st.expon,st.exponnorm,st.exponweib,st.exponpow,st.f,st.fatiguelife,st.fisk,
-    #         st.foldcauchy,st.foldnorm,st.frechet_r,st.frechet_l,st.genlogistic,st.genpareto,st.gennorm,st.genexpon,
-    #         st.genextreme,st.gausshyper,st.gamma,st.gengamma,st.genhalflogistic,st.gilbrat,st.gompertz,st.gumbel_r,
-    #         st.gumbel_l,st.halfcauchy,st.halflogistic,st.halfnorm,st.halfgennorm,st.hypsecant,st.invgamma,st.invgauss,
-    #         st.invweibull,st.johnsonsb,st.johnsonsu,st.ksone,st.kstwobign,st.laplace,st.levy,st.levy_l,st.levy_stable,
-    #         st.logistic,st.loggamma,st.loglaplace,st.lognorm,st.lomax,st.maxwell,st.mielke,st.nakagami,st.ncx2,st.ncf,
-    #         st.nct,st.norm,st.pareto,st.pearson3,st.powerlaw,st.powerlognorm,st.powernorm,st.rdist,st.reciprocal,
-    #         st.rayleigh,st.rice,st.recipinvgauss,st.semicircular,st.t,st.triang,st.truncexpon,st.truncnorm,st.tukeylambda,
-    #         st.uniform,st.vonmises,st.vonmises_line,st.wald,st.weibull_min,st.weibull_max,st.wrapcauchy
-    #     ]
-    # =============================================================================
-     
-        # Common Distributions for Insurance claims cost:          
-    # =============================================================================
-    #         DISTRIBUTIONS = [ st.expon, st.beta, st.burr, st.halfnorm, st.pareto, st.genpareto, st.invweibull, st.gamma,
-    #                           st.gengamma, st.uniform, st.norm]
-    # =============================================================================
-                   
         # Best holders
         best_distribution = st.norm
         best_params = (0.0, 1.0)
         best_sse = np.inf
     
         # Estimate distribution parameters from data
-        for distribution in DISTRIBUTIONS:
+        for distribution in dist:
             
             # Try to fit the distribution
             try:
@@ -91,7 +66,7 @@ class fit_distribution():
     
         return (best_distribution.name, best_params)
     
-    def make_pdf(self, dist, params, size=10000):
+    def make_pdf(self, distribution, params, size=10000):
         """Generate distributions's Probability Distribution Function """
     
         # Separate parts of parameters
@@ -99,27 +74,27 @@ class fit_distribution():
         loc = params[-2]
         scale = params[-1]
     
-        # Get sane start and end points of distribution
-        start = dist.ppf(0.01, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.01, loc=loc, scale=scale)
-        end = dist.ppf(0.99, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.99, loc=loc, scale=scale)
+        # Get same start and end points of distribution
+        start = distribution.ppf(0.01, *arg, loc=loc, scale=scale) if arg else distribution.ppf(0.01, loc=loc, scale=scale)
+        end = distribution.ppf(0.99, *arg, loc=loc, scale=scale) if arg else distribution.ppf(0.99, loc=loc, scale=scale)
     
         # Build PDF and turn into pandas Series
         x = np.linspace(start, end, size)
-        y = dist.pdf(x, loc=loc, scale=scale, *arg)
+        y = distribution.pdf(x, loc=loc, scale=scale, *arg)
         pdf = pd.Series(y, x)
     
         return pdf
     
     
-    def compare(self,dist):
+    def compare(self,dist_name):
         # unpack
-        DISTRIBUTIONS=[]
-        for each in dist:
+        dist=[]
+        for each in dist_name:
             ls = getattr(st, each)
-            DISTRIBUTIONS.append(ls)
+            dist.append(ls)
   
-        if len(DISTRIBUTIONS) ==1:
-            best_fit_name, best_fit_params = self.best_fit_distribution(self.data, DISTRIBUTIONS, 250)
+        if len(dist) ==1:
+            best_fit_name, best_fit_params = self.best_fit_distribution(self.data, dist, 250)
            
         else:
             # Plot for comparison distribution
@@ -129,7 +104,7 @@ class fit_distribution():
             dataYLim = ax.get_ylim()
             
             # Find best fit distribution
-            best_fit_name, best_fit_params = self.best_fit_distribution(self.data, DISTRIBUTIONS, 250, ax)
+            best_fit_name, best_fit_params = self.best_fit_distribution(self.data, dist, 250, ax)
             
             # Update plots
             ax.set_ylim(dataYLim)
